3f79327521373464f82d4dda3d2f4d80c1f86c8a
Vitaliy Bashkir <vabashkir@icloud.com>
2025-12-22 13:42:26 +0200

    fix controller

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,6 +1,7 @@
+import createHttpError from 'http-errors';
 import Note from '../models/note.js';
 
-// GET /notes
+// GET /notes  (з пагінацією та фільтрацією)
 export const getAllNotes = async (req, res, next) => {
   try {
     const {
@@ -38,6 +39,69 @@ export const getAllNotes = async (req, res, next) => {
       totalPages,
       notes,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// GET /notes/:noteId
+export const getNoteById = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+
+    const note = await Note.findById(noteId);
+
+    if (!note) {
+      throw createHttpError(404, 'Note not found');
+    }
+
+    res.status(200).json(note);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// POST /notes
+export const createNote = async (req, res, next) => {
+  try {
+    const note = await Note.create(req.body);
+    res.status(201).json(note);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// DELETE /notes/:noteId
+export const deleteNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+
+    const deleted = await Note.findByIdAndDelete(noteId);
+
+    if (!deleted) {
+      throw createHttpError(404, 'Note not found');
+    }
+
+    res.status(200).json(deleted);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// PATCH /notes/:noteId
+export const updateNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+
+    const updated = await Note.findByIdAndUpdate(noteId, req.body, {
+      new: true,
+    });
+
+    if (!updated) {
+      throw createHttpError(404, 'Note not found');
+    }
+
+    res.status(200).json(updated);
   } catch (error) {
     next(error);
   }
